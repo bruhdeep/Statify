a8d8adafe0b99cee00800925e2c5f170d8f59f33
Pradip Shrestha <shresthapradeep47@gmail.com>
2024-03-03 12:10:48 +0545

    build fix

--- app/api/spotifyregister/route.ts
+++ app/api/spotifyregister/route.ts
@@ -2,14 +2,9 @@ import connect from "@/utils/db";
 import User from "@/models/User";
 import { NextResponse } from "next/server";
 
-export async function POST(request: {
-  json: () =>
-    | PromiseLike<{ name: any; email: any }>
-    | { name: any; email: any };
-}) {
+export const POST = async (request: any) => {
   const { name, email } = await request.json();
   await connect();
   await User.create({ name, email });
   return NextResponse.json({ message: "user is registered", status: 201 });
-}
-    
+};
