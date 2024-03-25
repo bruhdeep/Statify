967d7c5170b721b77d245e3d3b1278de709b98af
Pradip Shrestha <shresthapradeep47@gmail.com>
2024-03-25 14:21:42 +0545

    test

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -1,20 +1,4 @@
-import User from "@/models/User";
-import connect from "@/utils/db";
+import register from "./options"; // Update the path accordingly
 
-export async function register(email) {
-  try {
-    //connect to mongo
-    await connect();
-    const existingUser = await User.findOne({ email });
-    //check exist ing user
-    if (existingUser) {
-      console.log("Email already exists in MongoDB:", email);
-    } else {
-      const user = new User({ email });
-      await user.save();
-      console.log("Email saved to MongoDB:", email);
-    }
-  } catch (error) {
-    console.error("Error saving email to MongoDB:", error);
-  }
-}
+const handler = register;
+export { handler as GET, handler as POST };
