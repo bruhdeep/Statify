aa85b9875555475198a7463d4b45880b45a54d3a
Pradip Shrestha <shresthapradeep47@gmail.com>
2024-04-13 15:46:55 +0545

    test

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -26,6 +26,7 @@ async function refreshAccessToken(token) {
   const params = new URLSearchParams();
   params.append("grant_type", "refresh_token");
   params.append("refresh_token", token.refreshToken);
+  params.append("client_id", process.env.SPOTIFY_CLIENT_ID);
   const response = await fetch("https://accounts.spotify.com/api/token", {
     method: "POST",
     headers: {
