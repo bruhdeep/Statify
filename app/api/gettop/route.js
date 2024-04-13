95f193500f31282d7f856ee40f84de6502bda32d
Pradip Shrestha <shresthapradeep47@gmail.com>
2024-04-13 15:46:09 +0545

    added top artists for mongo

---
+++ app/api/gettop/route.js
@@ -0,0 +1,48 @@
+import TopArtistAndTrack from "@/models/Top";
+import connect from "@/utils/db";
+
+export async function GET(request) {
+  const { searchParams } = new URL(
+    request.url,
+    `http://${request.headers.get("host")}`
+  );
+  const query = searchParams.get("query");
+
+  try {
+    await connect();
+
+    const topArtistAndTrack = await TopArtistAndTrack.findOne({
+      user_email: query,
+    });
+
+    if (!topArtistAndTrack) {
+      return new Response(
+        JSON.stringify("No top artists and tracks found for the user."),
+        {
+          status: 404,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+    }
+
+    return new Response(JSON.stringify(topArtistAndTrack), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error fetching top artists and tracks:", error);
+    return new Response(
+      JSON.stringify("Error fetching top artists and tracks:"),
+      {
+        status: 400,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+}
---
+++ models/Top.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const topSchema = new Schema(
+  {
+    user_email: { type: String, required: true },
+    top_artists: [
+      {
+        name: { type: String, required: true },
+        image: { type: String, required: true },
+        popularity: { type: Number, required: true },
+      },
+    ],
+    top_tracks: [
+      {
+        name: { type: String, required: true },
+        image: { type: String, required: true },
+        artists: { type: [String], required: true },
+        popularity: { type: Number, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.Top || mongoose.model("Top", topSchema);
