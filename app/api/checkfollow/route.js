3439186ff05e248c4746cd426778ffd3277fe977
Pradip Shrestha <shresthapradeep47@gmail.com>
2024-04-22 12:28:50 +0545

    smol fix

--- app/api/checkfollow/route.js
+++ app/api/checkfollow/route.js
@@ -17,8 +17,8 @@ export async function GET(request) {
     // Check if the user is already following the other user
     const follow = await Follow.findOne({ followerId, followeeId });
 
-    const followerexist = await User.findOne({ followerId });
-    const followeeexist = await User.findOne({ followeeId });
+    const followerexist = await User.findOne({ email: followerId });
+    const followeeexist = await User.findOne({ email: followeeId });
 
     if (!followerexist || !followeeexist) {
       return new Response(JSON.stringify({ error: "User not found" }), {
